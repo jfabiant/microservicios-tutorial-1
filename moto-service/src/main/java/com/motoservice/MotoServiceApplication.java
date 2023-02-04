8565d935b7af3840b3a7af7e8db639a60ee03fb9
jfabiant <jfabiantorres99@gmail.com>
2023-02-03 19:06:55 -0500

    Feign Client & Rest Template

--- moto-service/src/main/java/com/motoservice/MotoServiceApplication.java
+++ moto-service/src/main/java/com/motoservice/MotoServiceApplication.java
@@ -3,9 +3,11 @@ package com.motoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
