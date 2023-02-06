7893e0f67195d60bd24145e02ce5e5f2e391145f
jfabiant <jfabiantorres99@gmail.com>
2023-02-06 12:47:06 -0500

    Circuit Braker implementation

--- user-service/src/main/java/com/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/userservice/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.userservice.controllers;
 import com.userservice.domains.Car;
 import com.userservice.domains.Usuario;
 import com.userservice.services.UserService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,16 +46,19 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}/cars")
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallBackGetCars")
     public ResponseEntity<?> getCars(@PathVariable Long userId){
         return ResponseEntity.ok(this.userService.getCars(userId));
     }
     
     @GetMapping("/users/{userId}/motos")
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGetMotos")
     public ResponseEntity<?> getMotoList(@PathVariable Long userId){
         return ResponseEntity.ok(this.userService.getMotos(userId));
     }
 
     @PostMapping("/cars/{userId}")
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallBackSaveCar")
     public ResponseEntity<Car> saveNewCar(@PathVariable Long userId,
     		@RequestBody Car car) {
     	return ResponseEntity.ok(this.userService.saveCar(userId, car));
@@ -61,6 +67,22 @@ public class UserController {
     @GetMapping("/cars/{userId}")
     public ResponseEntity<List<Car>> getCarListByUserId(@PathVariable Long userId){
     	return ResponseEntity.ok(this.userService.getCarsByUserId(userId));
+    }
+    
+    public ResponseEntity<?> fallBackGetCars(@PathVariable Long userId){
+    	return ResponseEntity.ok("Los carros del usuario "+userId+" se encuentran en"
+    			+ " mantenimiento, por favor espere 24hrs.");
+    }
+    
+    public ResponseEntity<?> fallBackGetMotos(@PathVariable Long userId){
+    	return ResponseEntity.ok("Las motos del usuario "+userId+" se encuentran en"
+    			+ " mantenimiento, por favor espere 24hrs.");
+    }
+    
+    public ResponseEntity<?> fallBackSaveCar(@PathVariable Long userId,
+    		@RequestBody Car car){
+    	return ResponseEntity.ok("No se puede guardar el carro del usuario "+userId+" "
+    			+ "porque no tiene suficiente dinero en la tarjeta registrada.");
     }
     
 }
